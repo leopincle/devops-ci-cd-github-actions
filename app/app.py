50e9424e7057f9d0f3f4fb7987676529eaf0c0ea
LeoPincle <leo.soto965@gmail.com>
2026-03-24 09:47:31 -0600

    feat: update application message to validate CI/CD deployment

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return {"message": "CI/CD pipeline working 🚀"}
+    return {"message": "CI/CD EKS 🚀"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
